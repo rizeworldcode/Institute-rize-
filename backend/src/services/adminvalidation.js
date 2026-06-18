de645ad3ea372fc6b19ff10465c60f300ab5139e
rizeworldcode <rizeworldclient@gmail.com>
2026-06-18 17:55:59 +0530

    Update adminvalidation.js
    change gmail system

--- backend/src/services/adminvalidation.js
+++ backend/src/services/adminvalidation.js
@@ -4,11 +4,13 @@ const nodemailer = require("nodemailer");
 const admin_model = require("../models/adminmodel.js");
 
 const transporter = nodemailer.createTransport({
-    service: "Gmail",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 const sendOTP = (email, otp) => {
