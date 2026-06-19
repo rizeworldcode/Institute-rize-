6311bce8336ec9536efebdc38e08bb70c5ae73e0
rizeworldcode <rizeworldclient@gmail.com>
2026-06-19 09:41:37 +0530

    Update adminvalidation.js
    Update adminvalidation.js user mail or pass

--- backend/src/services/adminvalidation.js
+++ backend/src/services/adminvalidation.js
@@ -8,8 +8,8 @@ const transporter = nodemailer.createTransport({
   port: 587,
   secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    user: rizeworldcode@gmail.com,
+    pass: tzyj jabr qyid ynxb,
   },
 });
 
